bfffbb49847d456b40cd78c99da34f62f08448b5
Adrian <vargas.131251@caloocan.sti.edu.ph>
2024-04-04 22:33:57 +0800

    blank

--- src/sections/Layout.jsx
+++ src/sections/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+
 import Work from "../components/Work";
 const Layout = () => {
     return (
@@ -20,7 +21,5 @@ const Layout = () => {
         </div>
     );
 };
-
-
 
 export default Layout;
